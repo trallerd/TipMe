b0c4cc52bc85cde2edf54b174baa40355f3026a7
Trallerd <jessicacristec@gmail.com>
2020-12-05 14:02:52 -0300

    Tip

--- app/src/main/java/com/trallerd/gorjeta/MainActivity.kt
+++ app/src/main/java/com/trallerd/gorjeta/MainActivity.kt
@@ -2,6 +2,8 @@ package com.trallerd.gorjeta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,17 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         calcular.setOnClickListener {
-            val valor = valor.text.toString().toFloat()
-            val percentualGorjeta = gorjeta.text.toString().toFloat()
-            val pessoas = pessoa.text.toString().toInt()
+            if (!TextUtils.isEmpty(valor.text.toString())) {
+                if (!TextUtils.isEmpty(gorjeta.text.toString())) {
+                    if (!TextUtils.isEmpty(pessoa.text.toString())) {
+                        val valor = valor.text.toString().toFloat()
+                        val percentualGorjeta = gorjeta.text.toString().toFloat()
+                        val pessoas = pessoa.text.toString().toInt()
 
-            val gorjeta = valor * (percentualGorjeta / 100)
-            val conta = valor + gorjeta
-            val divisao = conta / pessoas
+                        val gorjeta = valor * (percentualGorjeta / 100)
+                        val conta = valor + gorjeta
+                        val divisao = conta / pessoas
 
-            resultGorjeta.text = "Gorjeta (R$): $gorjeta"
-            resultConta.text = "Total da Conta: R$ $conta"
-            resultPessoa.text = "Total de R$ $divisao por pessoa"
+                        resultGorjeta.text = "Gorjeta (R$): $gorjeta"
+                        resultConta.text = "Total da Conta: R$ $conta"
+                        resultPessoa.text = "Total de R$ $divisao por pessoa"
+                    } else {
+                        Toast.makeText(applicationContext,"Preencha todos os campos!!",Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(applicationContext,"Preencha todos os campos!!",Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(applicationContext,"Preencha todos os campos!!",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
